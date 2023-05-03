d08376433ecf473a38d3e0de977c67082524d552
onyulhaq <92550196+onyulhaq@users.noreply.github.com>
2023-05-02 22:12:28 -0700

    exercises and progress

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,10 @@
+let country = 'usa';
+let continent = 'NA';
+let population = '331 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Type Assignment
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,45 @@
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas')
+console.log('23')
+
+let firstName = 'Jonas'
+
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let years = 3
+
+let PI = 3.1415
+
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+// console.log(typeof true)
+console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'jonas')
+
+javascriptIsFun = 'YES!'
+
+console.log(typeof javascriptIsFun)
+
+
+let year
+
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
