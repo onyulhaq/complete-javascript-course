825559f4ab9051a25e904ea7cf6b9157761173df
onyulhaq <92550196+onyulhaq@users.noreply.github.com>
2023-05-09 22:08:17 -0700

    more practice

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,3 +1,5 @@
+/*
+
 let country = 'usa';
 let continent = 'NA';
 let population = '331 million';
@@ -6,5 +8,92 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-// Data Type Assignment
+////// Coding Challenge 1
+
+// Question 1
+
+let markMass = 78
+let markHeight = 1.69
+
+let johnMass = 92
+let johnHeight = 1.95
+
+// Question 2
+
+let markBMI = markMass / markHeight ** 2
+let johnBMI = johnMass / johnHeight ** 2
+
+// Question 3
+
+console.log(johnBMI, markBMI, markBMI > johnBMI)
+
+
+/////// Coding Challenge 2
+
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI ${markBMI} is higher than John's`)
+} else {
+    console.log(`John's BMI ${johnBMI} is higher than Mark's`)
+}
+
+
+///////// Coding Challenge 3
+
+// Question 1
+let dolphinAverage = (96 + 108 + 89) / 3
+let koalasAverage = (88 + 91 + 110) / 3
+
+//Question 2
+
+if (dolphinAverage > koalasAverage) {
+    console.log(`Dolphins win!`)
+} else if (dolphinAverage < koalasAverage) {
+    console.log(`Koalas win!`)
+} else if (dolphinAverage === koalasAverage) {
+    console.log(`Tie`)
+}
+
+// Bonus question 1
+
+dolphinAverage = (97 + 112 + 101) / 3
+koalasAverage = (109 + 95 + 123) / 3
+
+
+if (dolphinAverage > koalasAverage && dolphinAverage >= 100) {
+    console.log(`Dolphins win!`)
+} else if (dolphinAverage < koalasAverage && koalasAverage >= 100) {
+    console.log(`Koalas win!`)
+} else if (dolphinAverage === koalasAverage) {
+    console.log(`Tie`)
+}
+
+
+// Bonus Question 2
+
+
+dolphinAverage = (97 + 112 + 101) / 3
+koalasAverage = (109 + 95 + 106) / 3
+
+if (dolphinAverage > koalasAverage && dolphinAverage >= 100) {
+    console.log(`Dolphins win!`)
+} else if (dolphinAverage < koalasAverage && koalasAverage >= 100) {
+    console.log(`Koalas win!`)
+} else if (dolphinAverage === koalasAverage && dolphinAverage >= 100 &&
+    koalasAverage >= 100) {
+    console.log(`Tie`)
+} else {
+    console.log(`Nobody wins`)
+}
+
+
+///// Coding Challenges 4
+
+const bill = 275
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 :
+    bill * .2
+
+console.log(`your bill is ${bill + tip}`)
+*/
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -17,7 +17,6 @@ let years = 3
 
 let PI = 3.1415
 
-*/
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun)
@@ -43,3 +42,270 @@ console.log(year)
 console.log(typeof year)
 
 console.log(typeof null)
+
+
+
+
+
+let age = 30
+
+age = 31
+
+const birthYear = 1991
+
+// birthYear = 1992
+
+// const job
+
+var job = 'programmer'
+
+job = 'teacher'
+
+let lastName = 'Schmedtmann'
+console.log(lastName)
+
+
+
+// math operators
+const now = 2037;
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+const firstName = 'Jonas'
+const lastName = 'Schmedtmann'
+console.log(firstName + " " + lastName)
+
+// assignment operators
+let x = 10 + 5
+x += 10
+x *= 4
+x /= 20
+x++
+x--
+console.log(x)
+
+//comparison operators
+
+console.log(ageJonas > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+
+console.log(now - 1991 > now - 2018)
+
+const now = 2037
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+
+console.log(now - 1991 > now - 2018)
+
+
+let x, y;
+x = y = 25 - 10 - 5
+
+console.log(x, y)
+
+*/
+
+/*
+
+const firstName = 'Jonas'
+const job = 'teacher'
+const birthYear = 1991
+const year = 2037
+
+const jonas = "I'm " + firstName + ", a " + (year - birthYear) + " year-old teacher"
+
+console.log(jonas)
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`
+
+console.log(jonasNew)
+
+console.log(`just a regular string...`)
+
+console.log(`String
+multiple
+lines`)
+
+*/
+
+/*
+const age = 15
+
+if (age >= 18) {
+    console.log(`Sarah can start driving license 😎`)
+} else {
+    const yearsLeft = 18 - age
+    console.log(`Sarah is too young, Wait another ${yearsLeft}
+years :)`)
+}
+
+const birthYear = 2012
+
+let century
+
+if (birthYear <= 2000) {
+    century = 20
+} else {
+    century = 21
+}
+
+console.log(century)
+
+
+*/
+
+/*
+const inputYear = '1991'
+console.log(Number(inputYear), inputYear)
+console.log(Number(inputYear) + 18)
+
+
+console.log(Number("jonas"))
+console.log(typeof NaN)
+
+console.log(String(23), 23)
+
+// type coercion
+
+console.log('I am' + 23 + " years old")
+
+console.log('23' - "10" - 3)
+console.log('23' + "10" + 3)
+
+console.log("23" * "2")
+console.log("23" / "2")
+
+
+let n = "1" + 1
+
+n = n - 1
+
+console.log(n)
+
+
+
+*/
+
+/*
+
+
+const money = 0
+
+if (money) {
+    console.log(`Don't spend it all`)
+} else {
+    console.log(`You should get a job`)
+}
+
+*/
+/*
+const age = 18
+
+if (age === 18) {
+   console.log(`you just became an adult - strict`)
+}
+
+
+
+if (age == '18') {
+   console.log(`you just became an adult - loose`)
+}
+
+const favorite = Number(prompt("What is your favorite number?"))
+
+
+console.log(favorite, typeof favorite)
+
+if (favorite === 23) {
+   console.log(`Cool! 23 is an amazing number!`)
+} else if (favorite === 7) {
+   console.log(`Cool! 7 is also a cool number`)
+} else {
+   console.log(`Number is not 23 or 7`)
+}
+
+
+
+if (favorite !== 23) {
+   console.log(`Why not 23?`)
+}
+
+
+
+
+*/
+
+/*
+const hasDriverLicense = true
+const hasGoodVision = false
+
+console.log(hasDriverLicense && hasGoodVision)
+console.log(hasDriverLicense || hasGoodVision)
+
+const shouldDrive = hasDriverLicense && hasGoodVision
+
+
+const isTired = true
+
+console.log(hasDriverLicense || hasGoodVision || isTired)
+
+
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+    console.log(`Sarah is able to drive`)
+} else {
+    console.log(`Somebody else should drive`)
+}
+
+
+*/
+
+
+//////////////// Switch Statment
+
+/*
+const day = 'monday'
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log(`plan course structure`)
+        console.log('Go to coding meetup')
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos')
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples')
+        break
+    case 'friday':
+        console.log('Record video')
+        break
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend')
+        break
+    default:
+        console.log('Not a valid day')
+}
+
+*/
+
+///// Condition
+
+const age = 23
+
+age >= 18 ? console.log(`I like to drink 🍷`) :
+    console.log(`I like to drink water 💧💧`)
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧'
+console.log(drink)
+
+
